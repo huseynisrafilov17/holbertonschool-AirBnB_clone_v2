dbe13b49b88a06ac5986d7278e667fbc6c52241d
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-01 15:05:10 +0400

    6th task - some changes and adapting to ORM

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column, Integer, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state", cascade="delete")
+
+    @cities.getter
+    def cities():
+        from models import storage
+        objects = storage.all(City)
+        cities = []
+        for i in objects:
+            if State.id == objects[i].state_id:
+                cities.append(objects[i])
+        return cities
