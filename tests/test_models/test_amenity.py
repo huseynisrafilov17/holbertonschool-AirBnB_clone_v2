87210c8e1da5cb99a8533fe05c4429e3749450df
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-02 16:44:15 +0400

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,12 +8,13 @@ class test_Amenity(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Huseyn is the best groupmate"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
+        self.amenity1 = Amenity(name="Wi-fi")
 
     def test_name2(self):
-        """ """
+        """Huseyn is the best groupmate"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(self.amenity1.name), str)
