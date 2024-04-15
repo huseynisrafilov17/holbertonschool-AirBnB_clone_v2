c8e78308da0346df64fbfadd2631a00a848bcea7
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-15 18:14:21 +0400

    0th task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""My Flask Task"""
 from flask import Flask
-"""
-task 0
-"""
-
-
-app = Flask(__name__, strict_slashes=False)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello HBNB!"
+    return "<p>Hello, HBNB!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
