1003ec9e45e9144f40a3254ca64afd6592f0b7cf
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-17 17:41:22 +0400

    slight changes

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
