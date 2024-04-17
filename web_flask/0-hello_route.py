6805521b9e5bf3a81781f399714abbe9e21760d1
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-17 14:54:02 +0400

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ task 0
 
 app = Flask(__name__, strict_slashes=False)
 
+
 @app.route("/")
 def hello_world():
     """default page"""
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
