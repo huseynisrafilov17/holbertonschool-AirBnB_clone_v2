1a58cee4e16ec814f0bf341716674c29c7a8ef3d
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-20 13:04:28 +0400

    7th task

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Flask Project"""
+from models.__init__ import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    states = storage.all(State).values()
+    new_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', objs=new_states)
+
+
+@app.teardown_appcontext
+def close_db():
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run()
